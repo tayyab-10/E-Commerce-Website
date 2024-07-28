02bcfd65e3024e123b6cd8f6efd249df1e651aec
Muhammad Tayyab <tu.haider138@gmail.com>
2024-07-29 00:02:36 +0500

    Added all the CRUD Functions for the Product

--- Backend/Controllers/productController.js
+++ Backend/Controllers/productController.js
@@ -24,11 +24,11 @@ exports.getAllProducts=async(req,res)=>{
   })
 }
 
-//update products -- Admin can update the product only
+//Get a single product because a user can find a single product as well as admin
 
-exports.updateproduct=async(req,res)=>{
+exports.getsingleProduct=async(req,res)=>{
 
-    let product = Product.findById(req.params.id);
+    const product=await Product.findById(req.params.id);
 
     if(!product){
         return res.status(500).json({
@@ -36,7 +36,26 @@ exports.updateproduct=async(req,res)=>{
             message:"Product not found"
         })
     }
-    product-await Product.findByIdAndUpdate(req.params.id,req.body,{
+  
+    res.status(200).json({
+      success:true,
+      product
+    })
+  }
+
+//update products -- Admin can update the product only
+
+exports.updateproduct=async(req,res)=>{
+
+    let product = await Product.findById(req.params.id);   //we use let to change the current product
+
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        })
+    }
+    product=await Product.findByIdAndUpdate(req.params.id,req.body,{
         new:true,
         runValidators:true,
         useFindAndModify:false
@@ -45,4 +64,33 @@ exports.updateproduct=async(req,res)=>{
         success:true,
         product
     })
+}
+
+//Delete product -- Admin
+
+exports.deleteproduct=async(req,res)=>{
+
+    try {
+        const product = await Product.findById(req.params.id);
+
+        if (!product) {
+            return res.status(404).json({
+                success: false,
+                message: "Product not found"
+            });
+        }
+
+        await Product.findByIdAndDelete(req.params.id);
+
+        res.status(200).json({
+            success: true,
+            message: "Product deleted successfully"
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+
 }
--- Backend/Routes/ProductRoute.js
+++ Backend/Routes/ProductRoute.js
@@ -1,10 +1,15 @@
 const express=require("express");
-const { getAllProducts, createProduct } = require("../Controllers/productController");
+const { getAllProducts, createProduct, updateproduct, deleteproduct, getsingleProduct } = require("../Controllers/productController");
 
 const router=express.Router();
 
 
 router.route("/products").get(getAllProducts);
 router.route("/products/new").post(createProduct);
+router.route("/products/:id").put(updateproduct);  //we cannot get id from the auth token we have to give it in the url
+router.route("/products/:id").delete(deleteproduct);
+router.route("/products/:id").get(getsingleProduct);
+
+
 
 module.exports=router;
